26d584741e09dcb0ffc56a371b0fd6e35a621a10
floh1111 <floh1111@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2011-03-23 14:41:15 +0000

    Restructuring firmware directories, work in progress!

--- root_file_system/dir300b/etc/hostsupdate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-wget -T15 http://$(cat /etc/config/nodewatcher | grep url | awk '{ print $3 }' | sed -e "s/\]'//g" -e "s/'\[//g")/api_nodewatcher.php?section=get_hostnames_and_mac -O - | grep -v -e "^..-..-" | sort -u > /etc/bat-hosts
