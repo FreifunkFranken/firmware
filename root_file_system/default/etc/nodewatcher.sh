18d956bd74d793e02a3ae6c49b75d6b0fcc9f5f8
floh1111 <floh1111@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2011-08-14 12:18:23 +0000

    Fixed array counting of originators

--- root_file_system/default/etc/nodewatcher.sh
+++ root_file_system/default/etc/nodewatcher.sh
@@ -271,6 +271,7 @@ crawl() {
 					IFS="
 "
 					BAT_ADV_ORIGINATORS=`batctl o | awk '/O/ {next} /B/ {next} {print}'`
+					count=0;
 					for row in $BAT_ADV_ORIGINATORS; do
 						originator=`echo $row | awk '{print $1}'`
 						last_seen=`echo $row | awk '{print $2}'`
@@ -279,7 +280,8 @@ crawl() {
 						link_quality="${link_quality//(/}"
 						link_quality="${link_quality//)/}"
 						
-						batman_adv_originators=$batman_adv_originators"<batman_adv_originators><originator>$originator</originator><link_quality>$link_quality</link_quality><last_seen>$last_seen</last_seen></batman_adv_originators>"
+						batman_adv_originators=$batman_adv_originators"<batman_adv_originators><originator_$count><originator>$originator</originator><link_quality>$link_quality</link_quality><last_seen>$last_seen</last_seen></originator_$count></batman_adv_originators>"
+						count=$[$count+1]
 					done
 					IFS=$OLDIFS
 				fi
