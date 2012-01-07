abf48090bd9dc99169c0a27abb9ac1097d509762
bjo <bjo@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2012-01-07 16:52:45 +0000

    changed buildpatch-directory, added fonera

--- build_script.sh
+++ build_script.sh
@@ -21,8 +21,8 @@ prepare() {
 	cp ./build_patches/tinc/001_fix_a_few_small_memory_leaks.patch ./build_dir/feeds/packages/net/tinc/patches/001_fix_a_few_small_memory_leaks.patch
 
 	case "$1" in
-		"dir300")
-			svn export ./build_patches/dir300/260_fixdmaoffset.patch ./build_dir/target/linux/atheros/patches-2.6.30/260_fixdmaoffset.patch
+		"dir300" | "fonera")
+			svn export ./build_patches/ar23xx/260_fixdmaoffset.patch ./build_dir/target/linux/atheros/patches-2.6.30/260_fixdmaoffset.patch
 			;;
 	esac
 }
