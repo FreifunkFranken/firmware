5dd93bac695acc0ea15e28f460f7b9c49b348dd3
floh1111 <floh1111@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2012-01-11 22:59:03 +0000

    Merging changes on nodewatcher, configurator and hostsupdate from Trunk

--- root_file_system/default/etc/hostsupdate.sh
+++ root_file_system/default/etc/hostsupdate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget -T15 http://$(cat /etc/config/nodewatcher | grep url | awk '{ print $3 }' | sed -e "s/\]'//g" -e "s/'\[//g")/api_nodewatcher.php?section=get_hostnames_and_mac -O - | grep -v -e "^..-..-" | sort -u > /etc/bat-hosts
+wget -T15 http://[`uci get configurator.@api[0].ipv6_address`%`uci get configurator.@api[0].ipv6_interface`]/api_nodewatcher.php?section=get_hostnames_and_mac -O - | grep -v -e "^..-..-" | sort -u > /etc/bat-hosts
