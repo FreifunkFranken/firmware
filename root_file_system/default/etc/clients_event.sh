f0050578fe73d0d165da1b48c77f68b84ab4530a
floh1111 <floh1111@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2012-03-02 01:03:47 +0000

    Add bootup notification and dissasociation notification

--- root_file_system/default/etc/clients_event.sh
+++ root_file_system/default/etc/clients_event.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+i=`/etc/clients.sh`
+wget -q -O - http://status.kreativitaet-trifft-technik.de/update/ff\?bootup=`date +%s`\&clients_count=$i
+
 while read LINE
 do
-	if [ "`echo $LINE | grep 'wlan0:'`" != "" ]; then
-		wget -q -O - http://status.kreativitaet-trifft-technik.de/update/ff\?client_associated=`date +%s`\&clients_count=`/etc/clients.sh` & sleep 3; kill $!
-                #echo http://status.kreativitaet-trifft-technik.de/update/ff\?client_associated=`date +%s`\&clients_count=`/etc/clients.sh` & sleep 3; kill $!
-
-	fi
-	#echo `echo $LINE | grep 'wlan0:' | awk '{ print $4 }'` >> /var/log/clients_event.log
+	if [ "`echo $LINE | grep 'wlan0: new station'`" != "" ]; then
+		i=`expr $i + 1`
+		wget -q -O - http://status.kreativitaet-trifft-technik.de/update/ff\?client_associated=`date +%s`\&clients_count=$i
+        fi
+        if [ "`echo $LINE | grep 'wlan0: unknown event 20'`" != "" ]; then                   
+                i=`expr $i - 1` 
+                wget -q -O - http://status.kreativitaet-trifft-technik.de/update/ff\?client_disassociated=`date +%s`\&clients_count=$i
+        fi
 done
 
 
