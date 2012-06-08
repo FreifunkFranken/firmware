049ea9666a966ef6c341d51617c126416c0b3c2d
floh1111 <floh1111@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2012-06-08 11:30:22 +0000

    Add sources and script to build own libpcap

---
+++ flash_tools/fonera-flash/build_libpcap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tar xfvz ./libpcap-0.8.1.tar.gz
+cp ./libpcap-shared.patch ./libpcap-0.8.1/
+cd libpcap-0.8.1/
+patch -Np1 -i libpcap-shared.patch
+sed -i -e "s/@MAJOR@/`awk -F '.' '{ print $1 }' VERSION`/"          -e "s/@MINOR@/`awk -F '.' '{ print $2 }' VERSION`/"  -e "s/@SUBMINOR@/`awk -F '.' '{ print $3 }' VERSION`/"  -e "s/@VERSION@/`cat VERSION`/" Makefile.in
+./configure
+make
+mkdir pkg
+make DESTDIR=./pkg install 
+cd ..
