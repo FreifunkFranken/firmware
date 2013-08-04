8f58079510a31989e87f893abf049b36c7632d7b
Clemens John <clemens-john@gmx.de>
2013-08-04 16:58:53 +0000

    No code changes, just formatting. Replace whitespaces with tabs to indent

--- bsp/default/root_file_system/etc/configurator.sh
+++ bsp/default/root_file_system/etc/configurator.sh
@@ -27,24 +27,24 @@ fi
 API_RETRY=$(($API_RETRY - 1))
 
 if [[ $API_IPV4_ADRESS != "1" ]]; then
-    netmon_api=$API_IPV4_ADRESS
+	netmon_api=$API_IPV4_ADRESS
 else
-    netmon_api="[$API_IPV6_ADRESS"%"$API_IPV6_INTERFACE]"
+	netmon_api="[$API_IPV6_ADRESS"%"$API_IPV6_INTERFACE]"
 fi
 
 if [ $SCRIPT_ERROR_LEVEL -gt "1" ]; then
-    err() {
-        echo "$(date) [configurator]: $1" >> $SCRIPT_LOGFILE
-    }
+	err() {
+		echo "$(date) [configurator]: $1" >> $SCRIPT_LOGFILE
+	}
 else
-    err() {
-        :
-    }
+	err() {
+		:
+	}
 fi
 
 sync_hostname() {
 	err "Syncing hostname"
-    api_return=$(wget -T $API_TIMEOUT -q -O - "http://$netmon_api/api_csv_configurator.php?section=get_hostname&authentificationmethod=$CRAWL_METHOD&nickname=$CRAWL_NICKNAME&password=$CRAWL_PASSWORD&router_auto_update_hash=$CRAWL_UPDATE_HASH&router_id=$CRAWL_ROUTER_ID")
+	api_return=$(wget -T $API_TIMEOUT -q -O - "http://$netmon_api/api_csv_configurator.php?section=get_hostname&authentificationmethod=$CRAWL_METHOD&nickname=$CRAWL_NICKNAME&password=$CRAWL_PASSWORD&router_auto_update_hash=$CRAWL_UPDATE_HASH&router_id=$CRAWL_ROUTER_ID")
 	netmon_hostname=${api_return%,*}
 	netmon_hostname=${netmon_hostname#*,}
 	if [ "$netmon_hostname" != "" ]; then
@@ -54,7 +54,7 @@ sync_hostname() {
 			uci commit
 			echo $netmon_hostname > /proc/sys/kernel/hostname
 		else
-		    err "Hostname is up to date"
+			err "Hostname is up to date"
 		fi
 	fi
 }
@@ -63,30 +63,30 @@ assign_router() {
 	hostname=`cat /proc/sys/kernel/hostname`
 	
 	#Choose right login String
-    #Here maybe a ; to much at the end..??
-    login_strings=$(awk '{ mac=toupper($1); gsub(":", "", mac); printf mac ";" }' /sys/class/net/br-mesh/address /sys/class/net/eth0/address /sys/class/net/ath0/address 2> /dev/null)
-    ergebnis=$(wget -T $API_TIMEOUT -q -O - "http://$netmon_api/api_csv_configurator.php?section=test_login_strings&login_strings=$login_strings")
-    router_auto_assign_login_string=${ergebnis#*;}
-    ergebnis=${ergebnis%;*}
+	#Here maybe a ; to much at the end..??
+	login_strings=$(awk '{ mac=toupper($1); gsub(":", "", mac); printf mac ";" }' /sys/class/net/br-mesh/address /sys/class/net/eth0/address /sys/class/net/ath0/address 2> /dev/null)
+	ergebnis=$(wget -T $API_TIMEOUT -q -O - "http://$netmon_api/api_csv_configurator.php?section=test_login_strings&login_strings=$login_strings")
+	router_auto_assign_login_string=${ergebnis#*;}
+	ergebnis=${ergebnis%;*}
 	if [ "$ergebnis" = "error" ]; then
 		router_auto_assign_login_string=${login_strings%%;*}
-        err "A router with this login string does not exist: $login_strings"
-        err "Using $router_auto_assign_login_string as login string"
+		err "A router with this login string does not exist: $login_strings"
+		err "Using $router_auto_assign_login_string as login string"
 	fi
 
 	#Try to assign Router with choosen login string
-    ergebnis=$(wget -T $API_TIMEOUT -q -O - "http://$netmon_api/api_csv_configurator.php?section=router_auto_assign&router_auto_assign_login_string=$router_auto_assign_login_string&hostname=$hostname")
-    ret=${ergebnis%%;*}
-    errstr=${ergebnis#*;}
-    errstr=${errstr%%;*}
+	ergebnis=$(wget -T $API_TIMEOUT -q -O - "http://$netmon_api/api_csv_configurator.php?section=router_auto_assign&router_auto_assign_login_string=$router_auto_assign_login_string&hostname=$hostname")
+	ret=${ergebnis%%;*}
+	errstr=${ergebnis#*;}
+	errstr=${errstr%%;*}
 	if [ "$ret" != "success" ]; then
-        err "The router has not been assigned to a router in Netmon"
-        err "Failure on router_auto_assign: $errstr. Exiting"
-        exit 0
+		err "The router has not been assigned to a router in Netmon"
+		err "Failure on router_auto_assign: $errstr. Exiting"
+		exit 0
 	elif [ "$ret" = "success" ]; then
-        update_hash=${ergebnis%;*;*}
-        update_hash=${update_hash##*;}
-        api_key=${ergebnis##*;}
+		update_hash=${ergebnis%;*;*}
+		update_hash=${update_hash##*;}
+		api_key=${ergebnis##*;}
 		#write new config
 		uci set configurator.@crawl[0].router_id=$errstr
 		uci set configurator.@crawl[0].update_hash=$update_hash
@@ -94,7 +94,7 @@ assign_router() {
 		#set also new router id for nodewatcher
 		#uci set nodewatcher.@crawl[0].router_id=$errstr
 
-        err "The router $errstr has been assigned with a router in Netmon"
+		err "The router $errstr has been assigned with a router in Netmon"
 		uci commit
 
 		CRAWL_METHOD=`uci get configurator.@crawl[0].method`
@@ -107,18 +107,18 @@ assign_router() {
 
 autoadd_ipv6_address() {
 	err "Doing IPv6 autoadd"
-    ipv6_link_local_addr=$(ip addr show dev br-mesh scope link | awk '/inet6/{print $2}')
-    ipv6_link_local_netmask=${ipv6_link_local_addr##*/}
-    ipv6_link_local_addr=${ipv6_link_local_addr%%/*}
-    ergebnis=$(wget -T $API_TIMEOUT -q -O - "http://$netmon_api/api_csv_configurator.php?section=autoadd_ipv6_address&authentificationmethod=$CRAWL_METHOD&nickname=$CRAWL_NICKNAME&password=$CRAWL_PASSWORD&router_auto_update_hash=$CRAWL_UPDATE_HASH&router_id=$CRAWL_ROUTER_ID&ip=$ipv6_link_local_addr&netmask=$ipv6_link_local_netmask")
-    ret=${ergebnis%%,*}
+	ipv6_link_local_addr=$(ip addr show dev br-mesh scope link | awk '/inet6/{print $2}')
+	ipv6_link_local_netmask=${ipv6_link_local_addr##*/}
+	ipv6_link_local_addr=${ipv6_link_local_addr%%/*}
+	ergebnis=$(wget -T $API_TIMEOUT -q -O - "http://$netmon_api/api_csv_configurator.php?section=autoadd_ipv6_address&authentificationmethod=$CRAWL_METHOD&nickname=$CRAWL_NICKNAME&password=$CRAWL_PASSWORD&router_auto_update_hash=$CRAWL_UPDATE_HASH&router_id=$CRAWL_ROUTER_ID&ip=$ipv6_link_local_addr&netmask=$ipv6_link_local_netmask")
+	ret=${ergebnis%%,*}
 	if [ "$ret" = "success" ]; then
 		uci set configurator.@netmon[0].autoadd_ipv6_address='0'
 		uci commit
 		err "The IPv6 address of the router $CRAWL_ROUTER_ID has been added to the router in Netmon"
 		err "IPv6 Autoadd has been disabled cause it is no longer necesarry"
 	else
-        routerid=${ergebnis##*,}
+		routerid=${ergebnis##*,}
 		if [ "$routerid" == "$CRAWL_ROUTER_ID" ]; then
 			err "The IPv6 address already exists in Netmon on this router. Maybe because of a previos assignment"
 			uci set configurator.@netmon[0].autoadd_ipv6_address='0'
@@ -131,16 +131,16 @@ autoadd_ipv6_address() {
 }
 
 if [ $CRAWL_METHOD == "login" ]; then
-    err "Authentification method is: username and passwort"
+	err "Authentification method is: username and passwort"
 elif [ $CRAWL_METHOD == "hash" ]; then
-    err "Authentification method: autoassign and hash"
-    err "Checking if the router is already assigned to a router in Netmon"
+	err "Authentification method: autoassign and hash"
+	err "Checking if the router is already assigned to a router in Netmon"
 	if [ $CRAWL_UPDATE_HASH == "1" ]; then
-        err "The router is not assigned to a router in Netmon"
-        err "Trying to assign the router"
+		err "The router is not assigned to a router in Netmon"
+		err "Trying to assign the router"
 		assign_router
 	else
-        err "The router is already assigned to a router in Netmon"
+		err "The router is already assigned to a router in Netmon"
 	fi
 fi
 
@@ -149,5 +149,5 @@ if [[ $AUTOADD_IPV6_ADDRESS = "1" ]]; then
 fi
 
 if [[ $SCRIPT_SYNC_HOSTNAME = "1" ]]; then
-    sync_hostname
+	sync_hostname
 fi
