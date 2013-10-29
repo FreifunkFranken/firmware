8468b4c1302ebdb212bb0bf6d89bc4a8308bd0b7
Jan-Tarek Butt <buttjantarek@googlemail.com>
2013-10-30 00:11:36 +0100

    Einrückungen Korigiert
    Signed-off-by: Jan-Tarek Butt <buttjantarek@googlemail.com>

--- bsp/default/root_file_system/etc/clients_event.sh
+++ bsp/default/root_file_system/etc/clients_event.sh
@@ -8,11 +8,11 @@ do
 	if [ "`echo $LINE | grep 'wlan0: new station'`" != "" ]; then
 		i=`expr $i + 1`
 		wget -q -O - http://status.kreativitaet-trifft-technik.de/update/ff\?client_associated=`date +%s`\&clients_count=$i
-        fi
-        if [ "`echo $LINE | grep 'wlan0: unknown event 20'`" != "" ]; then
-                i=`expr $i - 1`
-                wget -q -O - http://status.kreativitaet-trifft-technik.de/update/ff\?client_disassociated=`date +%s`\&clients_count=$i
-        fi
+	fi
+	if [ "`echo $LINE | grep 'wlan0: unknown event 20'`" != "" ]; then
+		i=`expr $i - 1`
+		wget -q -O - http://status.kreativitaet-trifft-technik.de/update/ff\?client_disassociated=`date +%s`\&clients_count=$i
+	fi
 done
 
 
