eb60c91900ae157dbaa6e013b79780d985fb378c
Steffen Pankratz <kratz00@gmx.de>
2015-11-16 15:23:09 +0100

    - use value from /var/sysinfo/model instead of /var/sysinfo/board_name for model property
    Signed-off-by: Steffen Pankratz <kratz00@gmx.de>
    Reviewed-by: Tim Niemeyer <tim.niemeyer@mastersword.de>

--- bsp/default/root_file_system/etc/nodewatcher.sh
+++ bsp/default/root_file_system/etc/nodewatcher.sh
@@ -2,7 +2,7 @@
 # Netmon Nodewatcher (C) 2010-2012 Freifunk Oldenburg
 # License; GPL v3
 
-SCRIPT_VERSION="32"
+SCRIPT_VERSION="33"
 
 test -f /tmp/started || exit
 
@@ -68,7 +68,7 @@ crawl() {
         /model/ { printf "<cpu>"$2"</cpu>" }
         /system type/ { printf "<chipset>"$2"</chipset>" }
     ' /proc/cpuinfo)
-	model="<model>$(uci get board.model.name)</model>"
+	model="<model>$(cat /var/sysinfo/model)</model>"
 	local_time="`date +%s`"
 	load=$(awk '{ printf "<loadavg>"$3"</loadavg><processes>"$4"</processes>" }' /proc/loadavg)
 
