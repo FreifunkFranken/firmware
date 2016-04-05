ff7e8b7a6664c8247a635c1d92cf5d5c6928de58
Jan Kraus <mayosemmel@googlemail.com>
2016-04-05 22:00:32 +0200

    use custom names for binary builds. fixes #9
    also removed function board_clean from bsp files and board_postbuild from buildscript
    
    Signed-off-by: Jan Kraus <mayosemmel@gmail.com>
    
    suggested-by: Alexander Wunschik <freifunk@wunschik.net>
    Co-Authored-by: Alexander Wunschik <freifunk@wunschik.net>
    Co-Authored-by: Tobias Klaus <tk+ff@meskal.net>
    
    Reviewed-by: Tim Niemeyer <tim@tn-x.org>
    Reviewed-by: Tobias Klaus <tk+ff@meskal.net>

--- bsp/default/root_file_system/etc/sysupgrade.sh
+++ bsp/default/root_file_system/etc/sysupgrade.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
+cd /tmp/
 
-BOARD=`uci get board.model.name`
+. /etc/firmware_release
 
+BOARD=$(uci get board.model.name)
 case $BOARD in
     tl-wdr4900-v1 )
         SOC="mpc85xx" ;;
     * )
         SOC="ar71xx" ;;
 esac
-
-FILE="openwrt-${SOC}-generic-${BOARD}-squashfs-sysupgrade.bin"
-
 echo -ne "\nHardware: $BOARD\n"
+
+wget $(uci get firmware.upgrade.path)/release.nfo
+if [ ! -f release.nfo ]; then
+  echo -ne "Latest release information not found. Please try to update manually.\n\n"
+  exit 1
+fi
+VERSION=$(cat release.nfo|awk -F: '/VERSION:/ { print $2 }')
+rm -f release.nfo
+echo -ne "Firmware found on server: $VERSION\n"
+
+FILE="${FIRMWARE_COMMUNITY}-${VERSION}-${SOC}-generic-${BOARD}-squashfs-sysupgrade.bin"
 echo -ne "Downloading $FILE\n\n"
-cd /tmp/
 wget $(uci get firmware.upgrade.path)/${FILE}
 wget $(uci get firmware.upgrade.path)/${FILE}.md5
+
 echo -ne "\ndone. Comparing md5 sums: "
 md5sum -c ${FILE}.md5
 ret=$?
