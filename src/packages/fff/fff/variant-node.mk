f0eae4fff4b55827228f980d800d004b4b085c69
Tim Niemeyer <tim@tn-x.org>
2019-02-03 14:06:08 +0100

    buildscript: support to choose the build variant
    Signed-off-by: Tim Niemeyer <tim@tn-x.org>
    Reviewed-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

---
+++ src/packages/fff/fff/variant-node.mk
@@ -0,0 +1,15 @@
+define Package/fff-default
+    SECTION:=base
+    CATEGORY:=Freifunk
+    DEFAULT:=y
+    TITLE:=Freifunk-Franken Base default switcher
+    URL:=http://www.freifunk-franken.de
+    DEPENDS:=+fff-node
+endef
+
+define Package/fff-default/description
+    This package is used to switch on of the Freifunk Franken
+    package on per default
+endef
+
+$(eval $(call BuildPackage,fff-default))
