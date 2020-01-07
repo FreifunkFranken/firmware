91808ad710401cb39a36d08f1c18cd092a5aa432
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2020-01-07 14:40:24 +0100

    nodewatcher: Use Makefile version for nodewatcher data
    So far, nodewatcher version has to be specified twice, once in the
    Makefile and again in the nodewatcher script.
    
    With this patch, a version file is created during build than can
    be read in nodewatcher script, so version only has to be specified
    once anymore. The file name mimics the version files already present
    for OpenWrt and our firmware.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    Reviewed-by: Fabian Bläse <fabian@blaese.de>

--- src/packages/fff/fff-nodewatcher/files/usr/lib/nodewatcher.d/10-systemdata.sh
+++ src/packages/fff/fff-nodewatcher/files/usr/lib/nodewatcher.d/10-systemdata.sh
@@ -3,7 +3,7 @@
 # License; GPL v3
 
 SCRIPT_STATUS_FILE=$(uci get nodewatcher.@script[0].status_text_file)
-SCRIPT_VERSION="58"
+SCRIPT_VERSION=$(cat /etc/nodewatcher_version)
 
 debug() {
 	(>&2 echo "$1")
